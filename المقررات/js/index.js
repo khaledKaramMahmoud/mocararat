81e69d0f2942e33d02fded5bc208b843e4c2e20d
khaledKaramMahmoud <87311403+khaledKaramMahmoud@users.noreply.github.com>
2022-10-14 23:59:06 +0200

    Update index.js

--- المقررات/js/index.js
+++ المقررات/js/index.js
@@ -1,16 +1,40 @@
-const loginbtn=document.getElementById("loginbtn");
-const logincard=document.getElementById("logincard");
-const regbtn=document.getElementById("regbtn");
-const regcard=document.getElementById("regcard");
+const loginbtn = document.getElementById("loginbtn");
+const logincard = document.getElementById("logincard");
+const regbtn = document.getElementById("regbtn");
+const regcard = document.getElementById("regcard");
 
+loginbtn.onclick = () => {
+    logincard.style.display = "block";
+    regcard.style.display = "none";
+};
 
-loginbtn.onclick=()=>{
-    logincard.style.display = 'block'
-    regcard.style.display = 'none'
+regbtn.onclick = () => {
+    logincard.style.display = "none";
+    regcard.style.display = "block";
+};
+
+function ValidateEmail(inputText) {
+    var mailformat = /^\d{10}@+(s.mu.edu.sa)+$/;
+    if (inputText.value.match(mailformat)) {
+        alert("Valid email address!");
+        document.form1.text1.focus();
+        return true;
+    } else {
+        alert("You have entered an invalid email address!");
+        document.form1.text1.focus();
+        return false;
+    }
 }
 
-regbtn.onclick=()=>{
-    logincard.style.display = 'none'
-    regcard.style.display = 'block'
+function ValidateEmail2(inputText) {
+    var mailformat = /^\d{10}@+(s.mu.edu.sa)+$/;
+    if (inputText.value.match(mailformat)) {
+        alert("Valid email address!");
+        document.form1.text1.focus();
+        return true;
+    } else {
+        alert("You have entered an invalid email address!");
+        document.form1.text1.focus();
+        return false;
+    }
 }
-
